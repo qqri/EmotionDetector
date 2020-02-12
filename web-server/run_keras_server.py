4a80f474e8115979f4ad8bcd83c90d086f64b693
qqri <50269335+qqri@users.noreply.github.com>
2020-02-12 17:03:59 +0900

    init : kearas & flask server init // 출처추가

--- web-server/run_keras_server.py
+++ web-server/run_keras_server.py
@@ -1,3 +1,4 @@
+# 출처 : https://github.com/jrosebr1/simple-keras-rest-api
 from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
