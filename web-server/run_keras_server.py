12df5c6691faf3d76da2eb523ec8326534c67684
qqri <50269335+qqri@users.noreply.github.com>
2020-02-26 22:39:28 +0900

    bug : keras-web connect JSONERROR 수정
    JSONDecodeError run_keras_server.py
    수정

--- web-server/run_keras_server.py
+++ web-server/run_keras_server.py
@@ -6,17 +6,23 @@ from PIL import Image
 import numpy as np
 import flask
 import io
+import tensorflow as tf
 
 # initialize our Flask application and the Keras model
+from tensorboard.plugins import graph
+
 app = flask.Flask(__name__)
 model = None
+graph = []
 
 def load_model():
 	# load the pre-trained Keras model (here we are using a model
 	# pre-trained on ImageNet and provided by Keras, but you can
 	# substitute in your own networks just as easily)
+	global graph
 	global model
 	model = ResNet50(weights="imagenet")
+	graph = tf.get_default_graph()
 
 def prepare_image(image, target):
 	# if the image mode is not RGB, convert it
@@ -50,7 +56,8 @@ def predict():
 
 			# classify the input image and then initialize the list
 			# of predictions to return to the client
-			preds = model.predict(image)
+			with graph.as_default():
+				preds = model.predict(image)
 			results = imagenet_utils.decode_predictions(preds)
 			data["predictions"] = []
 
