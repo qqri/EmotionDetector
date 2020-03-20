a4cf029837b1670ab7f8f34abd39ea2ac998e97c
qqri <50269335+qqri@users.noreply.github.com>
2020-03-20 15:52:25 +0900

    content 데이터 접근

--- web-server/boardsite/api/models.py
+++ web-server/boardsite/api/models.py
@@ -1,11 +1,22 @@
 # api/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=144)
     subtitle = models.CharField(max_length=144, blank=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    #body = RichTextUploadingField()
     def __str__(self):
-        return '[{}] {}'.format(self.user.username, self.title)
+        return '[{}] {}'.format(self.user , self.title)
+
+    def post_save(self):
+        self.save()
+
+    #이유는 모르겠는데 이거하면 안됨
+    # class Meta:
+    #     managed = False
+    #     db_table = 'Post'
--- web-server/boardsite/api/views.py
+++ web-server/boardsite/api/views.py
@@ -1,7 +1,7 @@
 import os
 
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse, Http404
@@ -9,20 +9,7 @@ from django.template.loader import get_template
 from .models import Post
 from django.views import generic
 
-from rest_framework import viewsets, status
-from .serializers import PostSerializer
-from rest_framework import permissions
-import pickle
-import os
-from django.conf import settings
 
-import MeCab
-from keras_preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-maxlen=10
-max_words = 3000
-tokenizer = Tokenizer(num_words=max_words)
-m = MeCab.Tagger()
 
 
 def index(request):
@@ -30,37 +17,71 @@ def index(request):
 
 
 class IndexView(generic.ListView):
+    model = Post
     template_name = 'api/index_post.html'
     context_object_name = 'posts'
 
     def get_queryset(self):
-        return Post.objects.order_by('created_at')[:5]
+        return Post.objects.order_by('created_at')[:20]
+
 
 class DetailView(generic.DetailView):
     model = Post
     template_name = 'api/detail_post.html'
 
 
-class PostView(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), settings.MODEL_ROOT)
+from .forms import CreatePost
 
-        def getSequences(sentence):
-            sentence = [x.split("\t")[0] for x in m.parse(sentence).split("\n") if not x == "EOS" and not x == ""]
-            return pad_sequences(tokenizer.texts_to_sequences([sentence]), maxlen=maxlen)
+def CreatePostView(request):
+    if request.method == 'POST':
+        form = CreatePost(request.POST)
 
-        model = pickle.load(open(path, 'rb'))
-        sentence = serializer.data['content']
+        if form.is_valid():
+            # print( form.cleaned_data['content'] )
+            # print( type(form.cleaned_data['content']) ) -> content 데이터 접근
+            form.save()
+            return redirect('/api/diary/')
+        else:
+            return redirect('/api/diary/create')
+    else:
+        form = CreatePost()
+        return render(request, 'api/create_post.html', {'form': form})
 
-        test = "날씨가 좋다"
-        test = [x.split("\t")[0] for x in m.parse(test).split("\n") if not x == "EOS" and not x == ""]
-        print(pad_sequences(tokenizer.texts_to_sequences([test]), maxlen=maxlen))
+# from rest_framework import viewsets, status
+# from .serializers import PostSerializer
+# from rest_framework import permissions
+# import pickle
+# import os
+# from django.conf import settings
+#
+# import MeCab
+# from keras_preprocessing.sequence import pad_sequences
+# from keras.preprocessing.text import Tokenizer
+# maxlen=10
+# max_words = 3000
+# tokenizer = Tokenizer(num_words=max_words)
+# m = MeCab.Tagger()
 
-        data = model.predict_classes(getSequences(sentence))
-        #return Response(model.predict_classes( getSequences(sentence)),status=status.HTTP_200_OK)
-        return HttpResponse(data)
+# class PostView(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = (permissions.IsAuthenticated,)
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+#         path = os.path.join(os.path.dirname(os.path.realpath(__file__)), settings.MODEL_ROOT)
+#
+#         def getSequences(sentence):
+#             sentence = [x.split("\t")[0] for x in m.parse(sentence).split("\n") if not x == "EOS" and not x == ""]
+#             return pad_sequences(tokenizer.texts_to_sequences([sentence]), maxlen=maxlen)
+#
+#         model = pickle.load(open(path, 'rb'))
+#         sentence = serializer.data['content']
+#
+#         test = "날씨가 좋다"
+#         test = [x.split("\t")[0] for x in m.parse(test).split("\n") if not x == "EOS" and not x == ""]
+#         print(pad_sequences(tokenizer.texts_to_sequences([test]), maxlen=maxlen))
+#
+#         data = model.predict_classes(getSequences(sentence))
+#         #return Response(model.predict_classes( getSequences(sentence)),status=status.HTTP_200_OK)
+#         return HttpResponse(data)
 
